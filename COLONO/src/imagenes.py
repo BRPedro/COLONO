100680f2b2dfe2139d0d39883c46b5b318f5c636
PBR <PBR@DESKTOP-FMSQ9M9>
2016-09-16 10:07:35 -0600

    Inicio programa

---
+++ COLONO/src/imagenes.py
@@ -0,0 +1,14 @@
+from Tkinter import *
+from tkFileDialog import *
+from PIL import ImageTk, Image
+import cv2
+
+n=Tk()
+
+class listaImagens:
+    def __init__(self):
+
+        self.lista=[ImageTk.PhotoImage(Image.open("null.jpg")),PhotoImage(file="logo.gif"),PhotoImage(file="logo.gif")]
+    n.destroy()
+
+n.mainloop()
